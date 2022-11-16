a560af4661e805c24edfa637676b35fd0bb358c7
tejas salunkhe <salunkhetejas755@gmail.com>
2022-11-16 11:24:07 +0530

    Set

--- src/CollectionFramework/SetDemo.java
+++ src/CollectionFramework/SetDemo.java
@@ -1,6 +1,7 @@
 package CollectionFramework;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 //set doesn't maintain the insertion order
@@ -16,6 +17,19 @@ public class SetDemo {
         set.add(30);
         set.add(40);
         System.out.println(set);
+
+        //LinkedHashSet:-
+        //it maintains the insertion order.
+        
+        Set<Integer> set1 = new LinkedHashSet<>();
+        
+        set1.add(10);
+        set1.add(20);
+        set1.add(30);
+        set1.add(40);
+        set1.add(50);
+
+        System.out.println(set1);
 
     }
 }
