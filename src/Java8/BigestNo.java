401e3b610e80460268db1149812ffcb8f6bfe254
tejas salunkhe <salunkhetejas755@gmail.com>
2022-12-01 10:48:11 +0530

    BigestNo

--- src/Java8/BigestNo.java
+++ src/Java8/BigestNo.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class BigestNo {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
-        int max =0;
-        for (int i=0; i< list.size(); i++){
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
 
-//            if (list.get(i)>max){
-//                max = list.get(i);
+            if (list.get(i) > max) {
+                max = list.get(i);
 
+            }
         }
-        System.out.println("max=>"+max);
+            System.out.println("max=>" + max);
     }
 }
